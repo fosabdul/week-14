9b8b6ce0658c472aff34e50c203c6770c6d52cdb
fosabdul <um0364nu@go.minnstate.edu>
2021-12-15 16:22:28 -0600

    lab14 done

---
+++ src/components/StatesVisited.vue
@@ -0,0 +1,49 @@
+<template>
+
+<div> 
+  <h2> States you have visited</h2>
+
+  <p v-for="state in statesvisited" v-bind:key="state.name">
+      {{ state.name}}
+  </p>
+  
+</div>
+</template>
+
+<script>
+
+
+
+export default {
+
+  name: 'statesvisited', 
+  data() {
+      return {
+          statesvisited: [] 
+      }
+  },
+  mounted() {
+          this.$stateService.getVisited().then(StatesVisited => {
+              this.statesvisited = StatesVisited
+          }).catch(err => {
+              alert('sorry unable to fetch lis of states you visited')
+              console.error('error getting all the states',err)
+          }) // this where you get all the states
+      
+      
+  }
+}
+</script>
+
+<style scoped>
+
+.state-list-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+}
+
+.state-container {
+    margin: 1rem;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import StateList from '@/components/StateList'
+import AboutSite from '@/components/AboutSite'
+import StateMap from '@/components/StateMap'
+import NotFound from '@/components/NotFound'
+import StatesVisited from '@/components/StatesVisited'
+
+export default createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'StateList',
+            component: StateList
+        },
+        {
+            path: '/about',
+            name: 'AboutSite',
+            component: AboutSite
+        },
+        {
+            path: '/map/:state',
+            name: 'StateMap',
+            component: StateMap
+        },
+        {
+            path: '/I_have_been',
+            name: 'StatesVisited',
+            component: StatesVisited
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'NotFound',
+            component: NotFound
+        },
+        
+    ]
+})
